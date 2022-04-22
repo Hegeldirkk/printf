1fc93783e1c27f304a5c69c350dd107ca30fe6b6
NorthAlone <norbertlawadankossi@gmail.com>
2022-04-22 23:18:46 +0000

    test task 0 fails reordering root folder

--- test/main.c
+++ test/main.c
@@ -8,17 +8,10 @@
  */
 int main(void)
 {
-    int len = 0;
-    int len2 = 0;
     
-
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
     _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
     return (0);
 }
